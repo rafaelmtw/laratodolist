1e9a5af70319ac0e392f380d6fe3b3867e0953e4
Beaumont Yun <b.yun@hotmail.com>
2022-08-17 13:45:20 +0800

    Added todo list table / model / controller

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+use Inertia\Inertia;
+
+class TodoController extends Controller
+{
+    // public function index() {
+    //     $todo = Todo::all();
+
+    //     return Inertia('Dashboard', [
+
+    //     ]);
+    // }
+}
---
+++ app/Models/Todo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Todo extends Model
+{
+    use HasFactory, Notifiable;
+    /**
+     * Inverse one-to-many relationship to define who this model belongs to.
+     * https://laravel.com/docs/9.x/eloquent-relationships#one-to-many-inverse
+     * 
+     * @var string
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'priority',
+        'completed'
+    ];
+
+     /**
+     * The event map for the model.
+     * https://laravel.com/docs/9.x/eloquent#events
+     * 
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'created' => TeamCreated::class,
+        'updated' => TeamUpdated::class,
+        'deleted' => TeamDeleted::class,
+    ];
+}
